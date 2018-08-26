08d1bc5031729e6aaca2e6389752fb882ddaf9f6
Cap Van Vu <capvanvu@gmail.com>
2018-08-26 19:59:43 +0700

    Addons

---
+++ web_one2many_kanban/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Web one2many Kanban',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
+    'maintainer': 'Serpent Consulting Services Pvt. Ltd.',
+    'website': 'http://www.serpentcs.com',
+    'version': '9.0.0.2.0',
+    'category': 'Tools',
+    'license': 'AGPL-3',
+    'summary': 'Display one2many widget as kanban',
+    'description': 'Display one2many widget as kanban',
+    'sequence': 6,
+    'application': True,
+    'data': ['view/templates.xml'],
+    "images": ["static/description/web-one-kanban-banner.png"],
+    'installable': True,
+    'auto_install': False,
+}
